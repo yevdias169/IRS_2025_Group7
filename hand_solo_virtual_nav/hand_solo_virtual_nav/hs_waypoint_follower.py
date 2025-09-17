2add19be2bed1498f3ce8886845654e0556314a5
Yevin <yevindias@protonmail.com>
2025-09-17 17:27:09 +1000

    creating the package 2

---
+++ hand_solo_virtual_nav/hand_solo_virtual_nav/hs_waypoint_follower.py
@@ -0,0 +1,6 @@
+def main():
+    print('Hi from hand_solo_virtual_nav.')
+
+
+if __name__ == '__main__':
+    main()
---
+++ hand_solo_virtual_nav/setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+package_name = 'hand_solo_virtual_nav'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='yevin',
+    maintainer_email='yevindias@protonmail.com',
+    description='TODO: Package description',
+    license='Apache-2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'hs_waypoint_follower = hand_solo_virtual_nav.hs_waypoint_follower:main'
+        ],
+    },
+)
